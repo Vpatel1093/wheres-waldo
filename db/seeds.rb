3da3234495c7ef5e58c24e5db6abc36401dc98c2
Vishal <v.patel1093@gmail.com>
2017-10-25 13:47:41 -0400

    create puzzles controller and puzzle model, add puzzle images to assets, set up index and show pages

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Puzzle.create(name: 'Easy', image_url: 'easy.jpg')
+Puzzle.create(name: 'Medium', image_url: 'medium.jpg')
+Puzzle.create(name: 'Hard', image_url: 'hard.jpg')
